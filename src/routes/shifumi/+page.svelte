66601e0ccd96e2a350210df159886c027576f050
Bekiboo <julien.connault@gmail.com>
2023-07-03 20:41:39 +0300

    BROKEN - AI logic

--- src/routes/shifumi/+page.svelte
+++ src/routes/shifumi/+page.svelte
@@ -1,43 +1,86 @@
 <script lang="ts">
 	let playerScore = 0;
 	let computerScore = 0;
-	let round = 0;
-	let winner = '';
-	let playerChoice = '';
-	let computerChoice = '';
 
-	const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
-
-	const computerPlay = () => {
-		return choices[Math.floor(Math.random() * choices.length)];
+	type Round = {
+		playerChoice: Weapon;
+		computerChoice: Weapon;
+		result: 'You win' | 'You lose' | 'Tie';
 	};
 
-	const playRound = (playerSelection: string, computerSelection: string) => {
-		if (playerSelection === computerSelection) {
-			return 'Tie';
-		} else if (
-			(playerSelection === 'rock' && computerSelection === 'scissors') ||
-			(playerSelection === 'rock' && computerSelection === 'lizard') ||
-			(playerSelection === 'paper' && computerSelection === 'rock') ||
-			(playerSelection === 'paper' && computerSelection === 'spock') ||
-			(playerSelection === 'scissors' && computerSelection === 'paper') ||
-			(playerSelection === 'scissors' && computerSelection === 'lizard') ||
-			(playerSelection === 'lizard' && computerSelection === 'paper') ||
-			(playerSelection === 'lizard' && computerSelection === 'spock') ||
-			(playerSelection === 'spock' && computerSelection === 'rock') ||
-			(playerSelection === 'spock' && computerSelection === 'scissors')
-		) {
-			return 'You win';
-		} else {
-			return 'You lose';
+	const MAX_LOSING_ROUNDS_BEFORE_STRATEGY_CHANGE = 3;
+
+	let rounds: Round[] = [];
+	$: round = rounds.length + 1;
+
+	const strategies = [
+		() => {
+			console.log('Function 1');
+		},
+		() => {
+			console.log('Function 2');
+		},
+		() => {
+			console.log('Function 3');
+		}
+	];
+
+	let previousStrategyIndex = -1;
+	let currentStrategyIndex = -1;
+
+	let strategy: '1' | '2' | '3' = '1';
+
+	const winningCombinations: Record<string, string[]> = {
+		rock: ['scissors', 'lizard'],
+		paper: ['rock', 'spock'],
+		scissors: ['paper', 'lizard'],
+		lizard: ['paper', 'spock'],
+		spock: ['rock', 'scissors']
+	};
+
+	type Weapon = 'rock' | 'paper' | 'scissors' | 'lizard' | 'spock';
+	const weapons: Weapon[] = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+	let playerChoice: Weapon;
+	let computerChoice: Weapon;
+
+	const computerPlay = () => {
+		// First round, go random
+		if (rounds.length === 0) {
+			return weapons[Math.floor(Math.random() * weapons.length)];
+		}
+
+		if (rounds.length > MAX_LOSING_ROUNDS_BEFORE_STRATEGY_CHANGE) {
+			// get random strategy excluding the one already selected
+			const availableStrategies = strategies.filter((s) => s !== strategy);
+
+			// select a random strategy
+			function selectRandomStrategy() {
+				previousStrategyIndex = currentStrategyIndex;
+				while (currentStrategyIndex === previousStrategyIndex) {
+					currentStrategyIndex = Math.floor(Math.random() * strategies.length);
+				}
+				const selectedStrategy = strategies[currentStrategyIndex];
+				selectedStrategy();
+			}
+
+			// Step 3: Use a condition to trigger the function selection
+			function checkCondition() {
+				// Example condition: select a new function when a random number is greater than 0.5
+				const randomNumber = Math.random();
+				if (randomNumber > 0.5) {
+					selectRandomStrategy();
+				}
+			}
 		}
 	};
 
-	const updateScore = (result: string) => {
-		if (result === 'You win') {
-			playerScore++;
-		} else if (result === 'You lose') {
-			computerScore++;
+	const playRound = (playerChoice: Weapon, computerSelection: Weapon) => {
+		if (playerChoice === computerSelection) {
+			return 'Tie';
+		} else if (winningCombinations[playerChoice]?.includes(computerSelection)) {
+			return 'You win';
+		} else {
+			return 'You lose';
 		}
 	};
 
@@ -49,14 +92,14 @@
 		playerScore = 0;
 		computerScore = 0;
 		round = 0;
-		winner = '';
+		rounds = [];
 	};
 
-	const playGame = (playerSelection: string) => {
-		playerChoice = playerSelection;
+	const playGame = (playerChoice: Weapon) => {
 		computerChoice = computerPlay();
 		const result = playRound(playerChoice, computerChoice);
-		updateScore(result);
+		rounds.push({ playerChoice, computerChoice, result });
+		// updateScore(result);
 		updateRound();
 	};
 </script>
@@ -76,7 +119,6 @@
 	</div>
 	<div class="flex flex-col items-center">
 		<h2>Round {round}</h2>
-		<!-- <h2>{winner}</h2> -->
 		<div class="score color-red-500">
 			<h3>Player: {playerScore}</h3>
 			<h3>Computer: {computerScore}</h3>
